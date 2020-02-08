81dff43bb36f209dde9f23cc1aea1ab01a16764a
ancoknamhay <ancoknamhay@gmail.com>
2020-02-08 20:55:46 +0700

    Add files via upload

---
+++ ads-i18n.js
@@ -0,0 +1,86 @@
+'use strict';
+
+if (mejs.i18n.ca !== undefined) {
+	mejs.i18n.ca['mejs.ad-skip'] = 'Salta publicitat';
+	mejs.i18n.ca['mejs.ad-skip-info'] = ['Salta en 1 segon', 'Salta en %1 segons'];
+}
+if (mejs.i18n.cs !== undefined) {
+	mejs.i18n.cs['mejs.ad-skip'] = 'PÅ™eskoÄit reklamu';
+	mejs.i18n.cs['mejs.ad-skip-info'] = ['PÅ™eskoÄit za 1 sekundu', 'PÅ™eskoÄte za %1 sekundy', 'PÅ™eskoÄte za %1 sekund'];
+}
+if (mejs.i18n.de !== undefined) {
+	mejs.i18n.de['mejs.ad-skip'] = 'Werbung Ã¼berspringen';
+	mejs.i18n.de['mejs.ad-skip-info'] = ['Ãœberspringen in 1 Sekunde', 'Ãœberspringen in %1 Sekunden'];
+}
+if (mejs.i18n.es !== undefined) {
+	mejs.i18n.es['mejs.ad-skip'] = 'Saltar publicidad';
+	mejs.i18n.es['mejs.ad-skip-info'] = ['Saltar en 1 segundo', 'Saltar en %1 segundos'];
+}
+if (mejs.i18n.fa !== undefined) {
+	mejs.i18n.fa['mejs.ad-skip'] = 'Ù¾Ø±Ø´ ØªØ¨Ù„ÛŒØº';
+	mejs.i18n.fa['mejs.ad-skip-info'] = 'Ù¾Ø±Ø´ Ø¯Ø±1% Ø«Ø§Ù†ÛŒÙ‡';
+}
+if (mejs.i18n.fr !== undefined) {
+	mejs.i18n.fr['mejs.ad-skip'] = 'Passer la publicitÃ©';
+	mejs.i18n.fr['mejs.ad-skip-info'] = 'Passer la publicitÃ© dans %1 secondes';
+}
+if (mejs.i18n.hr !== undefined) {
+	mejs.i18n.hr['mejs.ad-skip'] = 'PreskoÄi oglas';
+	mejs.i18n.hr['mejs.ad-skip-info'] = ['PreskoÄi za 1 sekundu', 'PreskoÄi za %1 sekunde', 'PreskoÄi za %1 sekundi'];
+}
+if (mejs.i18n.hu !== undefined) {
+	mejs.i18n.hu['mejs.ad-skip'] = 'HirdetÃ©s Ã¡tugrÃ¡sa';
+	mejs.i18n.hu['mejs.ad-skip-info'] = ['UgrÃ¡s 1 mÃ¡sodperc alatt', 'UgrÃ¡s %1 mÃ¡sodpercen belÃ¼l'];
+}
+if (mejs.i18n.it !== undefined) {
+	mejs.i18n.it['mejs.ad-skip'] = 'Salta annuncio';
+	mejs.i18n.it['mejs.ad-skip-info'] = ['Salta in 1 secondo', 'Salta in %1 secondi'];
+}
+if (mejs.i18n.ja !== undefined) {
+	mejs.i18n.ja['mejs.ad-skip'] = 'åºƒå‘Šã‚’ã‚¹ã‚­ãƒƒãƒ—';
+	mejs.i18n.ja['mejs.ad-skip-info'] = '%1ç§’ã§ã‚¹ã‚­ãƒƒãƒ—ã™ã‚‹';
+}
+if (mejs.i18n.ko !== undefined) {
+	mejs.i18n.ko['mejs.ad-skip'] = 'ê´‘ê³ ë¥¼ ê±´ë„ˆ ë›¸ ìˆ˜';
+	mejs.i18n.ko['mejs.ad-skip-info'] = '%1 ì´ˆ í›„ ê±´ë„ˆ ë›°ê¸°';
+}
+if (mejs.i18n.nl !== undefined) {
+	mejs.i18n.nl['mejs.ad-skip'] = 'Ad overslaan';
+	mejs.i18n.nl['mejs.ad-skip-info'] = 'Overslaan in %1 seconden';
+}
+if (mejs.i18n.pl !== undefined) {
+	mejs.i18n.pl['mejs.ad-skip'] = 'PomiÅ„ reklamÄ™';
+	mejs.i18n.pl['mejs.ad-skip-info'] = ['PomiÅ„ za sekundÄ™', 'PomiÅ„ za %1 sekundy', 'PomiÅ„ za %1 sekund'];
+}
+if (mejs.i18n.pt !== undefined) {
+	mejs.i18n.pt['mejs.ad-skip'] = 'Pular AnÃºncio';
+	mejs.i18n.pt['mejs.ad-skip-info'] = ['Pular em 1 segundo', 'Pular em %1 segundos'];
+}
+if (mejs.i18n.ro !== undefined) {
+	mejs.i18n.ro['mejs.ad-skip'] = 'È˜tergeÈ›i anunÈ›ul';
+	mejs.i18n.ro['mejs.ad-skip-info'] = ['Treci peste 1 secundÄƒ', 'Treci peste %d secunde'];
+}
+if (mejs.i18n.ru !== undefined) {
+	mejs.i18n.ru['mejs.ad-skip'] = 'ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚ÑŒ Ñ€ÐµÐºÐ»Ð°Ð¼Ñƒ';
+	mejs.i18n.ru['mejs.ad-skip-info'] = ['ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚ÑŒ Ñ‡ÐµÑ€ÐµÐ· %1 ÑÐµÐºÑƒÐ½Ð´Ñƒ', 'ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚ÑŒ Ñ‡ÐµÑ€ÐµÐ· %1 ÑÐµÐºÑƒÐ½Ð´Ñ‹', 'ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚ÑŒ Ñ‡ÐµÑ€ÐµÐ· %1 ÑÐµÐºÑƒÐ½Ð´'];
+}
+if (mejs.i18n.sk !== undefined) {
+	mejs.i18n.sk['mejs.ad-skip'] = 'PreskoÄiÅ¥ reklamu';
+	mejs.i18n.sk['mejs.ad-skip-info'] = ['PreskoÄiÅ¥ za 1 sekundu', 'PreskoÄte za %1 sekundy', 'PreskoÄte za %1 sekÃºnd'];
+}
+if (mejs.i18n.sv !== undefined) {
+	mejs.i18n.sv['mejs.ad-skip'] = 'Hoppa Ã¶ver reklam';
+	mejs.i18n.sv['mejs.ad-skip-info'] = ['Hoppa in 1 second', 'Hoppa in %1 sekunder'];
+}
+if (mejs.i18n.uk !== undefined) {
+	mejs.i18n.uk['mejs.ad-skip'] = 'ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚Ð¸ Ñ€ÐµÐºÐ»Ð°Ð¼Ñƒ';
+	mejs.i18n.uk['mejs.ad-skip-info'] = ['ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚Ð¸ Ñ‡ÐµÑ€ÐµÐ· %1 ÑÐµÐºÑƒÐ½Ð´Ñƒ', 'ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚Ð¸ Ñ‡ÐµÑ€ÐµÐ· %1 ÑÐµÐºÑƒÐ½Ð´Ð¸', 'ÐŸÑ€Ð¾Ð¿ÑƒÑÑ‚Ð¸Ñ‚Ð¸ Ñ‡ÐµÑ€ÐµÐ· %1 ÑÐµÐºÑƒÐ½Ð´'];
+}
+if (mejs.i18n.zh !== undefined) {
+	mejs.i18n.zh['mejs.ad-skip'] = 'è·³éŽå»£å‘Š';
+	mejs.i18n.zh['mejs.ad-skip-info'] = 'è·³éŽ%1ç§’';
+}
+if (mejs.i18n['zh-CN'] !== undefined) {
+	mejs.i18n['zh-CN']['mejs.ad-skip'] = 'è·³è¿‡å¹¿å‘Š';
+	mejs.i18n['zh-CN']['mejs.ad-skip-info'] = 'è·³è¿‡%1ç§’';
+}
---
+++ speed-i18n.js
@@ -0,0 +1,65 @@
+'use strict';
+
+if (mejs.i18n.ca !== undefined) {
+	mejs.i18n.ca['mejs.speed-rate'] = 'Velocitat';
+}
+if (mejs.i18n.cs !== undefined) {
+	mejs.i18n.cs['mejs.speed-rate'] = 'Rychlost';
+}
+if (mejs.i18n.de !== undefined) {
+	mejs.i18n.de['mejs.speed-rate'] = 'Geschwindigkeitsrate';
+}
+if (mejs.i18n.es !== undefined) {
+	mejs.i18n.es['mejs.speed-rate'] = 'Velocidad';
+}
+if (mejs.i18n.fa !== undefined) {
+	mejs.i18n.fa['mejs.speed-rate'] = 'Ù†Ø±Ø® Ø³Ø±Ø¹Øª';
+}
+if (mejs.i18n.fr !== undefined) {
+	mejs.i18n.fr['mejs.speed-rate'] = 'Vitesse';
+}
+if (mejs.i18n.hr !== undefined) {
+	mejs.i18n.hr['mejs.speed-rate'] = 'Brzina reprodukcije';
+}
+if (mejs.i18n.hu !== undefined) {
+	mejs.i18n.hu['mejs.speed-rate'] = 'SebessÃ©g';
+}
+if (mejs.i18n.it !== undefined) {
+	mejs.i18n.it['mejs.speed-rate'] = 'VelocitÃ ';
+}
+if (mejs.i18n.ja !== undefined) {
+	mejs.i18n.ja['mejs.speed-rate'] = 'é«˜é€Ÿ';
+}
+if (mejs.i18n.ko !== undefined) {
+	mejs.i18n.ko['mejs.speed-rate'] = 'ì†ë„ ì†ë„';
+}
+if (mejs.i18n.nl !== undefined) {
+	mejs.i18n.nl['mejs.speed-rate'] = 'Snelheidsgraad';
+}
+if (mejs.i18n.pl !== undefined) {
+	mejs.i18n.pl['mejs.speed-rate'] = 'PrÄ™dkoÅ›Ä‡';
+}
+if (mejs.i18n.pt !== undefined) {
+	mejs.i18n.pt['mejs.speed-rate'] = 'Taxa de velocidade';
+}
+if (mejs.i18n.ro !== undefined) {
+	mejs.i18n.ro['mejs.speed-rate'] = 'VitezÄƒ de vitezÄƒ';
+}
+if (mejs.i18n.ru !== undefined) {
+	mejs.i18n.ru['mejs.speed-rate'] = 'Ð¡ÐºÐ¾Ñ€Ð¾ÑÑ‚ÑŒ Ð²Ð¾ÑÐ¿Ñ€Ð¾Ð¸Ð·Ð²ÐµÐ´ÐµÐ½Ð¸Ñ';
+}
+if (mejs.i18n.sk !== undefined) {
+	mejs.i18n.sk['mejs.speed-rate'] = 'RÃ½chlosÅ¥';
+}
+if (mejs.i18n.sv !== undefined) {
+	mejs.i18n.sv['mejs.speed-rate'] = 'Hastighet';
+}
+if (mejs.i18n.uk !== undefined) {
+	mejs.i18n.uk['mejs.speed-rate'] = 'Ð¨Ð²Ð¸Ð´ÐºÑ–ÑÑ‚ÑŒ Ð²Ñ–Ð´Ñ‚Ð²Ð¾Ñ€ÐµÐ½Ð½Ñ';
+}
+if (mejs.i18n.zh !== undefined) {
+	mejs.i18n.zh['mejs.speed-rate'] = 'é€Ÿåº¦';
+}
+if (mejs.i18n['zh-CN'] !== undefined) {
+	mejs.i18n['zh-CN']['mejs.speed-rate'] = 'é€Ÿåº¦';
+}
